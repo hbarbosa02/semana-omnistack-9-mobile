2d94936f205386ba8f4df79ee4c9a7a783566bfb
Hiran Gondim <hiran.g.b.silva@gmail.com>
2020-01-31 14:32:44 -0300

    Add web socket.

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import socketio from "socket.io-client";
 import {
+  Alert,
   SafeAreaView,
   StyleSheet,
   Image,
@@ -13,6 +15,22 @@ import logo from "../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffet(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.0.117:8000", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storagedTechs => {
